c71773753a8da4c0d8d83df0e5ae547978baf90a
Amir Ghezala <mohamedghezala@gmail.com>
2025-09-17 13:53:32 +0200

    add sidenav links

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,8 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
             src="/hero-desktop.png"
-            // width={1000}
-            // height={760}
+            width={1000}
+            height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
